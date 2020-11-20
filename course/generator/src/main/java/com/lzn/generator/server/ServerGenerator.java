e0310ba4e593d65e003cc6a57d3887e412848bd3
dutlzn <2436013662@qq.com>
2020-11-20 17:01:09 +0800

    生成器开发，代码生成器和mybatis generator的整合

--- course/generator/src/main/java/com/lzn/generator/server/ServerGenerator.java
+++ course/generator/src/main/java/com/lzn/generator/server/ServerGenerator.java
@@ -5,7 +5,12 @@ import com.lzn.generator.util.Field;
 import com.lzn.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.omg.CORBA.FREE_MEM;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
+import java.util.*;
 import java.util.*;
 
 import java.io.IOException;
@@ -15,11 +20,28 @@ public class ServerGenerator {
     static String toServicePath = "server\\src\\main\\java\\com\\lzn\\service\\";
     static String toControllerPath = MODULE + "\\src\\main\\java\\com\\lzn\\"+MODULE+"\\controller\\admin\\";
     static String toDtoPath = "server\\src\\main\\java\\com\\lzn\\dto\\";
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
     public static void main(String[] args) throws Exception {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
+// 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
         List<Field> fieldList = DbUtil.getColumnByTableName(domain);
         Set<String> typeSet = getJavaTypes(fieldList);
 
