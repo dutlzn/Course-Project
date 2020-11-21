2a6015be53aa622c115aac8dd428a5631aae26b9
dutlzn <2436013662@qq.com>
2020-11-21 21:06:41 +0800

    生成器升级，前端生成器升级，增加枚举字段的生成

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -12,11 +12,11 @@
       </button>
     </p>
 
-    <pagination ref="pagination" v-bind:list="list"></pagination>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
-    <table id="simple-table" class="table table-bordered table-hover">
+    <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
-        <tr>
+      <tr>
         <th>id</th>
         <th>名称</th>
         <th>概述</th>
@@ -28,282 +28,190 @@
         <th>状态</th>
         <th>报名数</th>
         <th>顺序</th>
-          <th>操作</th>
-        </tr>
+        <th>操作</th>
+      </tr>
       </thead>
 
       <tbody>
-        <tr v-for="course in courses">
+      <tr v-for="course in courses">
         <td>{{course.id}}</td>
         <td>{{course.name}}</td>
         <td>{{course.summary}}</td>
         <td>{{course.time}}</td>
         <td>{{course.price}}</td>
         <td>{{course.image}}</td>
-        <!-- <td>{{course.level}}</td> -->
-          <td>{{COURSE_LEVEL | optionKV(course.level)}}</td>
-            <td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>
-              <td>{{COURSE_STATUS | optionKV(course.status)}}</td>
-        <!-- <td>{{course.charge}}</td> -->
-        <!-- <td>{{course.status}}</td> -->
+        <td>{{COURSE_LEVEL | optionKV(course.level)}}</td>
+        <td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>
+        <td>{{COURSE_STATUS | optionKV(course.status)}}</td>
         <td>{{course.enroll}}</td>
         <td>{{course.sort}}</td>
-
-          <td>
-            <div class="hidden-sm hidden-xs btn-group">
-              <button v-on:click="edit(course)" class="btn btn-xs btn-info">
-                <i class="ace-icon fa fa-pencil bigger-120"></i>
-              </button>
-              <button
-                v-on:click="del(course.id)"
-                class="btn btn-xs btn-danger">
-                <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
-            </div>
-          </td>
-        </tr>
+      <td>
+        <div class="hidden-sm hidden-xs btn-group">
+          <button v-on:click="edit(course)" class="btn btn-xs btn-info">
+            <i class="ace-icon fa fa-pencil bigger-120"></i>
+          </button>
+          <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
+            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+          </button>
+        </div>
+      </td>
+      </tr>
       </tbody>
     </table>
 
-    <!-- 模态框 -->
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">表单</h4>
           </div>
           <div class="modal-body">
-            <!-- 表单 -->
-
             <form class="form-horizontal">
-
-                  <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">名称</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.name"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="名称"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="summary" class="col-sm-2 control-label">概述</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.summary"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="概述"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="time" class="col-sm-2 control-label">时长</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.time"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="时长"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="price" class="col-sm-2 control-label">价格（元）</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.price"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="价格（元）"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="image" class="col-sm-2 control-label">封面</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.image"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="封面"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="level" class="col-sm-2 control-label">级别</label>
-                        <div class="col-sm-10">
-                    <select v-model="course.level" class="form-control">
-                            <option v-for="o in COURSE_LEVEL" v-bind:value="o.key"> {{o.value}}</option>
-                          </select>
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="charge" class="col-sm-2 control-label">收费</label>
-                        <div class="col-sm-10">
-                       <select v-model="course.charge" class="form-control">
-                            <option v-for="o in COURSE_CHARGE" v-bind:value="o.key"> {{o.value}}</option>
-                          </select>
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="status" class="col-sm-2 control-label">状态</label>
-                        <div class="col-sm-10">
-                              <select v-model="course.status" class="form-control">
-                            <option v-for="o in COURSE_STATUS" v-bind:value="o.key"> {{o.value}}</option>
-                          </select>
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="enroll" class="col-sm-2 control-label">报名数</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.enroll"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="报名数"
-                          />
-                        </div>
-                      </div>
-                  <div class="form-group">
-                        <label for="sort" class="col-sm-2 control-label">顺序</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="course.sort"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="顺序"
-                          />
-                        </div>
-                      </div>
-
+              <div class="form-group">
+                <label class="col-sm-2 control-label">名称</label>
+                <div class="col-sm-10">
+                  <input v-model="course.name" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">概述</label>
+                <div class="col-sm-10">
+                  <input v-model="course.summary" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">时长</label>
+                <div class="col-sm-10">
+                  <input v-model="course.time" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">价格（元）</label>
+                <div class="col-sm-10">
+                  <input v-model="course.price" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">封面</label>
+                <div class="col-sm-10">
+                  <input v-model="course.image" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">级别</label>
+                <div class="col-sm-10">
+                  <select v-model="course.level" class="form-control">
+                    <option v-for="o in COURSE_LEVEL" v-bind:value="o.key">{{o.value}}</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">收费</label>
+                <div class="col-sm-10">
+                  <select v-model="course.charge" class="form-control">
+                    <option v-for="o in COURSE_CHARGE" v-bind:value="o.key">{{o.value}}</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">状态</label>
+                <div class="col-sm-10">
+                  <select v-model="course.status" class="form-control">
+                    <option v-for="o in COURSE_STATUS" v-bind:value="o.key">{{o.value}}</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">报名数</label>
+                <div class="col-sm-10">
+                  <input v-model="course.enroll" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">顺序</label>
+                <div class="col-sm-10">
+                  <input v-model="course.sort" class="form-control">
+                </div>
+              </div>
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">
-              取消
-            </button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">
-              保存
-            </button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
           </div>
-        </div>
-        <!-- /.modal-content -->
-      </div>
-      <!-- /.modal-dialog -->
-    </div>
-
-    <!-- /.modal -->
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
-</template>>
-
+</template>
 
 <script>
-import Pagination from "../../components/pagination.vue";
-
-export default {
-  name: "business-course",
-  components: { Pagination },
-
-  data: function () {
-    return {
-      // 映射表单数据
-      course: {},
-      courses: [],
-      COURSE_LEVEL:COURSE_LEVEL,
-      COURSE_STATUS:COURSE_STATUS,
-      COURSE_CHARGE:COURSE_CHARGE
-
-    };
-  },
-
-  // 页面初始化加载方法
-  mounted: function () {
-    // this.$parent.activeSidebar('business-course-sidebar');
-    let _this = this;
-    _this.$refs.pagination.size = 5;
-    _this.list(1);
-  },
-  methods: {
-    // 编辑课程
-    edit(course) {
-      let _this = this;
-      // _this.course = course;
-      _this.course = $.extend({}, course);
-      // 禁止点击空白的地方关闭
-      $(".modal").modal({
-        backdrop: "static",
-      });
-      $("#form-modal").modal("show");
+  import Pagination from "../../components/pagination";
+  export default {
+    components: {Pagination},
+    name: "business-course",
+    data: function() {
+      return {
+        course: {},
+        courses: [],
+        COURSE_LEVEL: COURSE_LEVEL,
+        COURSE_CHARGE: COURSE_CHARGE,
+        COURSE_STATUS: COURSE_STATUS,
+      }
     },
-    // 删除课程
-    del(id) {
+    mounted: function() {
       let _this = this;
+      _this.$refs.pagination.size = 5;
+      _this.list(1);
+      // sidebar激活样式方法一
+      // this.$parent.activeSidebar("business-course-sidebar");
 
-      Confirm.show("删除课程后不可恢复，确认删除？", function () {
+    },
+    methods: {
+      /**
+       * 点击【新增】
+       */
+      add() {
+        let _this = this;
+        _this.course = {};
+        $("#form-modal").modal("show");
+      },
+
+      /**
+       * 点击【编辑】
+       */
+      edit(course) {
+        let _this = this;
+        _this.course = $.extend({}, course);
+        $("#form-modal").modal("show");
+      },
+
+      /**
+       * 列表查询
+       */
+      list(page) {
+        let _this = this;
         Loading.show();
-        _this.$ajax
-          .delete(process.env.VUE_APP_SERVER + "/business/admin/course/delete/" + id)
-          .then((response) => {
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          });
-      });
-    },
-    // 新增课程
-    add() {
-      let _this = this;
-      // 禁止点击空白的地方关闭
-      _this.course = {};
-      $(".modal").modal({
-        backdrop: "static",
-      });
-      $("#form-modal").modal("show");
-    },
-
-    // 获取所有课程的数据
-    list(page) {
-      let _this = this;
-      Loading.show();
-      _this.$ajax
-        .post(process.env.VUE_APP_SERVER + "/business/admin/course/list", {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/list', {
           page: page,
-          //  跟组子组件获取  size应该有一个默认的参数
           size: _this.$refs.pagination.size,
-        })
-        .then((response) => {
+        }).then((response)=>{
           Loading.hide();
           let resp = response.data;
           _this.courses = resp.content.list;
           _this.$refs.pagination.render(page, resp.content.total);
-        });
-    },
 
-    // 保存课程的数据
-    save(page) {
-      let _this = this;
-     // 保存校验
+        })
+      },
+
+      /**
+       * 点击【保存】
+       */
+      save() {
+        let _this = this;
+
+        // 保存校验
         if (1 != 1
           || !Validator.require(_this.course.name, "名称")
           || !Validator.length(_this.course.name, "名称", 1, 50)
@@ -313,26 +221,37 @@ export default {
           return;
         }
 
-
-      Loading.show();
-      _this.$ajax
-        .post(
-          process.env.VUE_APP_SERVER + "/business/admin/course/save",
-          _this.course
-        )
-        .then((response) => {
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save', _this.course).then((response)=>{
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
-            // 关闭模态框
             $("#form-modal").modal("hide");
             _this.list(1);
-            Toast.success("保存成功");
+            Toast.success("保存成功！");
           } else {
-            Toast.warning(resp.message);
+            Toast.warning(resp.message)
           }
+        })
+      },
+
+      /**
+       * 点击【删除】
+       */
+      del(id) {
+        let _this = this;
+        Confirm.show("删除课程后不可恢复，确认删除？", function () {
+          Loading.show();
+          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/course/delete/' + id).then((response)=>{
+            Loading.hide();
+            let resp = response.data;
+            if (resp.success) {
+              _this.list(1);
+              Toast.success("删除成功！");
+            }
+          })
         });
-    },
-  },
-};
+      }
+    }
+  }
 </script>
--- course/generator/src/main/java/com/lzn/generator/ftl/vue.ftl
+++ course/generator/src/main/java/com/lzn/generator/ftl/vue.ftl
@@ -12,187 +12,155 @@
       </button>
     </p>
 
-    <pagination ref="pagination" v-bind:list="list"></pagination>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
-    <table id="simple-table" class="table table-bordered table-hover">
+    <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
-        <tr>
+      <tr>
 <#list fieldList as field>
           <#if field.nameHump!="createdAt" && field.nameHump!="updatedAt">
         <th>${field.nameCn}</th>
           </#if>
         </#list>
-          <th>操作</th>
-        </tr>
+        <th>操作</th>
+      </tr>
       </thead>
 
       <tbody>
-        <tr v-for="${domain} in ${domain}s">
+      <tr v-for="${domain} in ${domain}s">
         <#list fieldList as field>
           <#if field.nameHump!="createdAt" && field.nameHump!="updatedAt">
+            <#if field.enums>
+        <td>{{${field.enumsConst} | optionKV(${domain}.${field.nameHump})}}</td>
+            <#else>
         <td>{{${domain}.${field.nameHump}}}</td>
             </#if>
+          </#if>
         </#list>
-
-          <td>
-            <div class="hidden-sm hidden-xs btn-group">
-              <button v-on:click="edit(${domain})" class="btn btn-xs btn-info">
-                <i class="ace-icon fa fa-pencil bigger-120"></i>
-              </button>
-              <button
-                v-on:click="del(${domain}.id)"
-                class="btn btn-xs btn-danger">
-                <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
-            </div>
-          </td>
-        </tr>
+      <td>
+        <div class="hidden-sm hidden-xs btn-group">
+          <button v-on:click="edit(${domain})" class="btn btn-xs btn-info">
+            <i class="ace-icon fa fa-pencil bigger-120"></i>
+          </button>
+          <button v-on:click="del(${domain}.id)" class="btn btn-xs btn-danger">
+            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+          </button>
+        </div>
+      </td>
+      </tr>
       </tbody>
     </table>
 
-    <!-- 模态框 -->
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">表单</h4>
           </div>
           <div class="modal-body">
-            <!-- 表单 -->
-
             <form class="form-horizontal">
-
               <#list fieldList as field>
-                   <#if field.name!="id" && field.nameHump!="createdAt" && field.nameHump!="updatedAt">
-                  <div class="form-group">
-                        <label for="${field.nameHump}" class="col-sm-2 control-label">${field.nameCn}</label>
-                        <div class="col-sm-10">
-                          <input
-                            v-model="${domain}.${field.nameHump}"
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="${field.nameCn}"
-                          />
-                        </div>
-                      </div>
-                      </#if>
-                </#list>
-
+                <#if field.name!="id" && field.nameHump!="createdAt" && field.nameHump!="updatedAt">
+                  <#if field.enums>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">${field.nameCn}</label>
+                <div class="col-sm-10">
+                  <select v-model="${domain}.${field.nameHump}" class="form-control">
+                    <option v-for="o in ${field.enumsConst}" v-bind:value="o.key">{{o.value}}</option>
+                  </select>
+                </div>
+              </div>
+                  <#else>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">${field.nameCn}</label>
+                <div class="col-sm-10">
+                  <input v-model="${domain}.${field.nameHump}" class="form-control">
+                </div>
+              </div>
+                  </#if>
+                </#if>
+              </#list>
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">
-              取消
-            </button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">
-              保存
-            </button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
           </div>
-        </div>
-        <!-- /.modal-content -->
-      </div>
-      <!-- /.modal-dialog -->
-    </div>
-
-    <!-- /.modal -->
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
-</template>>
-
+</template>
 
 <script>
-import Pagination from "../../components/pagination.vue";
-
-export default {
-  name: "${module}-${domain}",
-  components: { Pagination },
-
-  data: function () {
-    return {
-      // 映射表单数据
-      ${domain}: {},
-      ${domain}s: [],
-    };
-  },
-
-  // 页面初始化加载方法
-  mounted: function () {
-    // this.$parent.activeSidebar('${module}-${domain}-sidebar');
-    let _this = this;
-    _this.$refs.pagination.size = 5;
-    _this.list(1);
-  },
-  methods: {
-    // 编辑${tableNameCn}
-    edit(${domain}) {
-      let _this = this;
-      // _this.${domain} = ${domain};
-      _this.${domain} = $.extend({}, ${domain});
-      // 禁止点击空白的地方关闭
-      $(".modal").modal({
-        backdrop: "static",
-      });
-      $("#form-modal").modal("show");
+  import Pagination from "../../components/pagination";
+  export default {
+    components: {Pagination},
+    name: "${module}-${domain}",
+    data: function() {
+      return {
+        ${domain}: {},
+        ${domain}s: [],
+        <#list fieldList as field>
+          <#if field.enums>
+        ${field.enumsConst}: ${field.enumsConst},
+          </#if>
+        </#list>
+      }
     },
-    // 删除${tableNameCn}
-    del(id) {
+    mounted: function() {
       let _this = this;
+      _this.$refs.pagination.size = 5;
+      _this.list(1);
+      // sidebar激活样式方法一
+      // this.$parent.activeSidebar("${module}-${domain}-sidebar");
 
-      Confirm.show("删除${tableNameCn}后不可恢复，确认删除？", function () {
+    },
+    methods: {
+      /**
+       * 点击【新增】
+       */
+      add() {
+        let _this = this;
+        _this.${domain} = {};
+        $("#form-modal").modal("show");
+      },
+
+      /**
+       * 点击【编辑】
+       */
+      edit(${domain}) {
+        let _this = this;
+        _this.${domain} = $.extend({}, ${domain});
+        $("#form-modal").modal("show");
+      },
+
+      /**
+       * 列表查询
+       */
+      list(page) {
+        let _this = this;
         Loading.show();
-        _this.$ajax
-          .delete(process.env.VUE_APP_SERVER + "/${module}/admin/${domain}/delete/" + id)
-          .then((response) => {
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          });
-      });
-    },
-    // 新增${tableNameCn}
-    add() {
-      let _this = this;
-      // 禁止点击空白的地方关闭
-      _this.${domain} = {};
-      $(".modal").modal({
-        backdrop: "static",
-      });
-      $("#form-modal").modal("show");
-    },
-
-    // 获取所有${tableNameCn}的数据
-    list(page) {
-      let _this = this;
-      Loading.show();
-      _this.$ajax
-        .post(process.env.VUE_APP_SERVER + "/${module}/admin/${domain}/list", {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/${module}/admin/${domain}/list', {
           page: page,
-          //  跟组子组件获取  size应该有一个默认的参数
           size: _this.$refs.pagination.size,
-        })
-        .then((response) => {
+        }).then((response)=>{
           Loading.hide();
           let resp = response.data;
           _this.${domain}s = resp.content.list;
           _this.$refs.pagination.render(page, resp.content.total);
-        });
-    },
 
-    // 保存${tableNameCn}的数据
-    save(page) {
-      let _this = this;
-     // 保存校验
+        })
+      },
+
+      /**
+       * 点击【保存】
+       */
+      save() {
+        let _this = this;
+
+        // 保存校验
         if (1 != 1
         <#list fieldList as field>
           <#if field.name!="id" && field.nameHump!="createdAt" && field.nameHump!="updatedAt" && field.nameHump!="sort">
@@ -208,26 +176,37 @@ export default {
           return;
         }
 
-
-      Loading.show();
-      _this.$ajax
-        .post(
-          process.env.VUE_APP_SERVER + "/${module}/admin/${domain}/save",
-          _this.${domain}
-        )
-        .then((response) => {
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/${module}/admin/${domain}/save', _this.${domain}).then((response)=>{
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
-            // 关闭模态框
             $("#form-modal").modal("hide");
             _this.list(1);
-            Toast.success("保存成功");
+            Toast.success("保存成功！");
           } else {
-            Toast.warning(resp.message);
+            Toast.warning(resp.message)
           }
+        })
+      },
+
+      /**
+       * 点击【删除】
+       */
+      del(id) {
+        let _this = this;
+        Confirm.show("删除${tableNameCn}后不可恢复，确认删除？", function () {
+          Loading.show();
+          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/${module}/admin/${domain}/delete/' + id).then((response)=>{
+            Loading.hide();
+            let resp = response.data;
+            if (resp.success) {
+              _this.list(1);
+              Toast.success("删除成功！");
+            }
+          })
         });
-    },
-  },
-};
+      }
+    }
+  }
 </script>
