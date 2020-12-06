114c40078678f8a86af4d2fd4c69f71862bf34a4
dutlzn <2436013662@qq.com>
2020-12-06 09:29:18 +0800

    文件上传功能开发：去除新增、编辑、删除功能

--- course-admin/src/views/admin/file.vue
+++ course-admin/src/views/admin/file.vue
@@ -8,21 +8,21 @@
 				</v-card-title>
 
 				<v-card-text>
-					 						<v-col cols="12">
-							<v-text-field label="相对路径" v-model="file.path" required></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-text-field label="文件名" v-model="file.name" required></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-text-field label="后缀" v-model="file.suffix" required></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-text-field label="大小" v-model="file.size" required></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-select label="用途" v-model="file.use" :items="FILE_USE" item-text="value" item-value="key"></v-select>
-						</v-col>
+					<v-col cols="12">
+						<v-text-field label="相对路径" v-model="file.path" required></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field label="文件名" v-model="file.name" required></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field label="后缀" v-model="file.suffix" required></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field label="大小" v-model="file.size" required></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-select label="用途" v-model="file.use" :items="FILE_USE" item-text="value" item-value="key"></v-select>
+					</v-col>
 
 				</v-card-text>
 				<v-card-actions>
@@ -41,11 +41,11 @@
 
 		<p class="ma-10">
 
-			<v-btn class="mr-5 mb-5" color="primary" @click="add()">
+<!-- 			<v-btn class="mr-5 mb-5" color="primary" @click="add()">
 				<v-icon left small>add</v-icon>
 				新增
 			</v-btn>
-
+ -->
 			<v-btn class="mb-5" color="primary" @click="list(1)">
 				<v-icon left small>refresh</v-icon>
 				刷新
@@ -63,27 +63,27 @@
 					<v-simple-table>
 						<thead>
 							<tr>
-										<th class="primary--text text-h6 text-center">id</th>
-										<th class="primary--text text-h6 text-center">相对路径</th>
-										<th class="primary--text text-h6 text-center">文件名</th>
-										<th class="primary--text text-h6 text-center">后缀</th>
-										<th class="primary--text text-h6 text-center">大小</th>
-										<th class="primary--text text-h6 text-center">用途</th>
-								<th class="primary--text  text-h6 text-center">
+								<th class="primary--text text-h6 text-center">id</th>
+								<th class="primary--text text-h6 text-center">相对路径</th>
+								<th class="primary--text text-h6 text-center">文件名</th>
+								<th class="primary--text text-h6 text-center">后缀</th>
+								<th class="primary--text text-h6 text-center">大小</th>
+								<th class="primary--text text-h6 text-center">用途</th>
+				<!-- 				<th class="primary--text  text-h6 text-center">
 									操作
-								</th>
+								</th> -->
 							</tr>
 						</thead>
 						<tbody>
 							<tr v-for="file in files" class="text-center">
 
-												<td>{{file.id}}</td>
-												<td>{{file.path}}</td>
-												<td>{{file.name}}</td>
-												<td>{{file.suffix}}</td>
-												<td>{{file.size}}</td>
-											<td>{{FILE_USE | optionKV(file.use)}}</td>
-
+								<td>{{file.id}}</td>
+								<td>{{file.path}}</td>
+								<td>{{file.name}}</td>
+								<td>{{file.suffix}}</td>
+								<td>{{file.size}}</td>
+								<td>{{FILE_USE | optionKV(file.use)}}</td>
+<!-- 
 								<td>
 									<v-row align="center" justify="space-around">
 										<v-btn x-small fab @click="edit(file)" class="primary">
@@ -94,7 +94,7 @@
 											<v-icon>delete</v-icon>
 										</v-btn>
 									</v-row>
-								</td>
+								</td> -->
 							</tr>
 						</tbody>
 					</v-simple-table>
@@ -115,43 +115,43 @@
 
 		data: function() {
 			return {
-	        file: {},
-	        files: [],
-						FILE_USE: FILE_USE_ARRAY,
+				file: {},
+				files: [],
+				FILE_USE: FILE_USE_ARRAY,
 
-					dialog: false,
+				dialog: false,
 			}
 		},
 
-	  mounted: function() {
+		mounted: function() {
 			let _this = this;
 			_this.$refs.pagination.size = 5;
 			_this.list(1);
 
-	  },
+		},
 
 		methods: {
 			// 获取所有小节的数据
 			list(page) {
-					let _this = this;
-					Loading.show();
-					_this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/list', {
-						page: page,
-						size: _this.$refs.pagination.size,
-					}).then((response)=>{
-						Loading.hide();
-						let resp = response.data;
-						_this.files = resp.content.list;
-						_this.$refs.pagination.render(page, resp.content.total);
+				let _this = this;
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/list', {
+					page: page,
+					size: _this.$refs.pagination.size,
+				}).then((response) => {
+					Loading.hide();
+					let resp = response.data;
+					_this.files = resp.content.list;
+					_this.$refs.pagination.render(page, resp.content.total);
 
-					})
+				})
 			},
 			/**
 			 * 新增小节
 			 */
 			add() {
-        let _this = this;
-        _this.file = {};
+				let _this = this;
+				_this.file = {};
 				_this.dialog = true;
 			},
 
@@ -159,58 +159,58 @@
 			 * 编辑小节
 			 */
 			edit(file) {
-        let _this = this;
-        _this.file = $.extend({}, file);
+				let _this = this;
+				_this.file = $.extend({}, file);
 				_this.dialog = true;
-      },
+			},
 
 			/**
 			 * 保存
 			 */
 			save() {
-        let _this = this;
+				let _this = this;
 
-        // 保存校验
-        if (1 != 1
-          || !Validator.require(_this.file.path, "相对路径")
-          || !Validator.length(_this.file.path, "相对路径", 1, 100)
-          || !Validator.length(_this.file.name, "文件名", 1, 100)
-          || !Validator.length(_this.file.suffix, "后缀", 1, 10)
-        ) {
-          return;
-        }
+				// 保存校验
+				if (1 != 1 ||
+					!Validator.require(_this.file.path, "相对路径") ||
+					!Validator.length(_this.file.path, "相对路径", 1, 100) ||
+					!Validator.length(_this.file.name, "文件名", 1, 100) ||
+					!Validator.length(_this.file.suffix, "后缀", 1, 10)
+				) {
+					return;
+				}
 
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save', _this.file).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if (resp.success) {
-            _this.dialog = false,
-            _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
-          }
-        })
-      },
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save', _this.file).then((response) => {
+					Loading.hide();
+					let resp = response.data;
+					if (resp.success) {
+						_this.dialog = false,
+							_this.list(1);
+						Toast.success("保存成功！");
+					} else {
+						Toast.warning(resp.message)
+					}
+				})
+			},
 
 			/**
 			 * 删除小节
 			 */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除文件后不可恢复，确认删除？", function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/file/admin/file/delete/' + id).then((response)=>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          })
-        });
-      }
+			del(id) {
+				let _this = this;
+				Confirm.show("删除文件后不可恢复，确认删除？", function() {
+					Loading.show();
+					_this.$ajax.delete(process.env.VUE_APP_SERVER + '/file/admin/file/delete/' + id).then((response) => {
+						Loading.hide();
+						let resp = response.data;
+						if (resp.success) {
+							_this.list(1);
+							Toast.success("删除成功！");
+						}
+					})
+				});
+			}
 		}
 	}
 </script>
