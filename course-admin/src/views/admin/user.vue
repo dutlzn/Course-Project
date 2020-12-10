cab34d216b0f3cb05d2843b82a7e7e5d027dc601
dutlzn <2436013662@qq.com>
2020-12-10 14:26:45 +0800

    增加修改密码

--- course-admin/src/views/admin/user.vue
+++ course-admin/src/views/admin/user.vue
@@ -8,16 +8,17 @@
 				</v-card-title>
 
 				<v-card-text>
-					 	<v-col cols="12">
-							<v-text-field label="登陆名" v-if="user.id" disabled v-model="user.loginName"></v-text-field>
-											<v-text-field label="登陆名" v-if="!user.id"  v-model="user.loginName"></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-text-field label="昵称" v-model="user.name" required></v-text-field>
-						</v-col>
-						<v-col cols="12">
-							<v-text-field label="密码" v-model="user.password" required></v-text-field>
-						</v-col>
+					<v-col cols="12">
+						<v-text-field label="登陆名" v-if="user.id" disabled v-model="user.loginName"></v-text-field>
+						<v-text-field label="登陆名" v-if="!user.id" v-model="user.loginName"></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field label="昵称" v-model="user.name" required></v-text-field>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field v-if="user.id" disabled label="密码" type="password" v-model="user.password"></v-text-field>
+						<v-text-field v-if="!user.id" label="密码" type="password" v-model="user.password" required></v-text-field>
+					</v-col>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
@@ -26,6 +27,31 @@
 					</v-btn>
 
 					<v-btn @click="save()" class="success">
+						保存
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+
+
+		<v-dialog v-model="dialogPassword" persistent max-width="300">
+			<v-card>
+				<v-card-title>
+					修改密码
+				</v-card-title>
+
+				<v-card-text>
+					<v-col cols="12">
+						<v-text-field label="密码" type="password" v-model="user.password" required></v-text-field>
+					</v-col>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn @click="dialogPassword = false" clas="info">
+						取消
+					</v-btn>
+
+					<v-btn @click="savePassword()" class="success">
 						保存
 					</v-btn>
 				</v-card-actions>
@@ -57,10 +83,10 @@
 					<v-simple-table>
 						<thead>
 							<tr>
-										<th class="primary--text text-h6 text-center">id</th>
-										<th class="primary--text text-h6 text-center">登陆名</th>
-										<th class="primary--text text-h6 text-center">昵称</th>
-										<th class="primary--text text-h6 text-center">密码</th>
+								<th class="primary--text text-h6 text-center">id</th>
+								<th class="primary--text text-h6 text-center">登陆名</th>
+								<th class="primary--text text-h6 text-center">昵称</th>
+								<th class="primary--text text-h6 text-center">密码</th>
 								<th class="primary--text  text-h6 text-center">
 									操作
 								</th>
@@ -69,15 +95,18 @@
 						<tbody>
 							<tr v-for="user in users" class="text-center">
 
-												<td>{{user.id}}</td>
-												<td>{{user.loginName}}</td>
-												<td>{{user.name}}</td>
-												<td>{{user.password}}</td>
+								<td>{{user.id}}</td>
+								<td>{{user.loginName}}</td>
+								<td>{{user.name}}</td>
+								<td>{{user.password}}</td>
 
 								<td>
 									<v-row align="center" justify="space-around">
 										<v-btn x-small fab @click="edit(user)" class="primary">
 											<v-icon>edit</v-icon>
+										</v-btn>
+										<v-btn x-small fab @click="editPassword(user)" class="info">
+											<v-icon>lock</v-icon>
 										</v-btn>
 
 										<v-btn x-small fab @click="del(user.id)" class="warning">
@@ -105,42 +134,43 @@
 
 		data: function() {
 			return {
-	        user: {},
-	        users: [],
+				user: {},
+				users: [],
 
-					dialog: false,
+				dialog: false,
+				dialogPassword: false,
 			}
 		},
 
-	  mounted: function() {
+		mounted: function() {
 			let _this = this;
 			_this.$refs.pagination.size = 5;
 			_this.list(1);
 
-	  },
+		},
 
 		methods: {
 			// 获取所有小节的数据
 			list(page) {
-					let _this = this;
-					Loading.show();
-					_this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/list', {
-						page: page,
-						size: _this.$refs.pagination.size,
-					}).then((response)=>{
-						Loading.hide();
-						let resp = response.data;
-						_this.users = resp.content.list;
-						_this.$refs.pagination.render(page, resp.content.total);
+				let _this = this;
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/list', {
+					page: page,
+					size: _this.$refs.pagination.size,
+				}).then((response) => {
+					Loading.hide();
+					let resp = response.data;
+					_this.users = resp.content.list;
+					_this.$refs.pagination.render(page, resp.content.total);
 
-					})
+				})
 			},
 			/**
 			 * 新增小节
 			 */
 			add() {
-        let _this = this;
-        _this.user = {};
+				let _this = this;
+				_this.user = {};
 				_this.dialog = true;
 			},
 
@@ -148,60 +178,103 @@
 			 * 编辑小节
 			 */
 			edit(user) {
-        let _this = this;
-        _this.user = $.extend({}, user);
+				let _this = this;
+				_this.user = $.extend({}, user);
 				_this.dialog = true;
-      },
+			},
 
 			/**
 			 * 保存
 			 */
 			save() {
-        let _this = this;
+				let _this = this;
 
-        // 保存校验
-        if (1 != 1
-          || !Validator.require(_this.user.loginName, "登陆名")
-          || !Validator.length(_this.user.loginName, "登陆名", 1, 50)
-          || !Validator.length(_this.user.name, "昵称", 1, 50)
-          || !Validator.require(_this.user.password, "密码")
-        ) {
-          return;
-        }
-				
+				// 保存校验
+				if (1 != 1 ||
+					!Validator.require(_this.user.loginName, "登陆名") ||
+					!Validator.length(_this.user.loginName, "登陆名", 1, 50) ||
+					!Validator.length(_this.user.name, "昵称", 1, 50) ||
+					!Validator.require(_this.user.password, "密码")
+				) {
+					return;
+				}
+
 				_this.user.password = hex_md5(_this.user.password + KEY);
-				
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save', _this.user).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if (resp.success) {
-            _this.dialog = false,
-            _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
-          }
-        })
-      },
+
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save', _this.user).then((response) => {
+					Loading.hide();
+					let resp = response.data;
+					if (resp.success) {
+						_this.dialog = false,
+							_this.list(1);
+						Toast.success("保存成功！");
+					} else {
+						Toast.warning(resp.message)
+					}
+				})
+			},
 
 			/**
 			 * 删除小节
 			 */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除用户后不可恢复，确认删除？", function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/system/admin/user/delete/' + id).then((response)=>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          })
-        });
-      }
+			del(id) {
+				let _this = this;
+				Confirm.show("删除用户后不可恢复，确认删除？", function() {
+					Loading.show();
+					_this.$ajax.delete(process.env.VUE_APP_SERVER + '/system/admin/user/delete/' + id).then((response) => {
+						Loading.hide();
+						let resp = response.data;
+						if (resp.success) {
+							_this.list(1);
+							Toast.success("删除成功！");
+						}
+					})
+				});
+			},
+
+			/**
+			 * 
+			 * 修改密码
+			 */
+
+			editPassword(user) {
+				let _this = this;
+				_this.user = $.extend({}, user);
+				_this.dialogPassword = true;
+			},
+
+			/**
+			 * 保存密码
+			 */
+			savePassword() {
+				let _this = this;
+
+				// 保存校验
+				if (1 != 1 ||
+					!Validator.require(_this.user.loginName, "登陆名") ||
+					!Validator.length(_this.user.loginName, "登陆名", 1, 50) ||
+					!Validator.length(_this.user.name, "昵称", 1, 50) ||
+					!Validator.require(_this.user.password, "密码")
+				) {
+					return;
+				}
+
+				_this.user.password = hex_md5(_this.user.password + KEY);
+
+				Loading.show();
+				_this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save-password', _this.user).then((response) => {
+					Loading.hide();
+					let resp = response.data;
+					if (resp.success) {
+						_this.dialogPassword = false,
+							_this.list(1);
+						Toast.success("保存成功！");
+					} else {
+						Toast.warning(resp.message)
+					}
+				})
+			}
 		}
 	}
 </script>
